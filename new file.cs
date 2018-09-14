37cbdb8dbb0bde940a77af111974ff8847ebdfa0
arunasekar <42831846+arunasekar@users.noreply.github.com>
2018-09-14 23:09:42 +0530

    Add files via upload

---
+++ new file.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream obj = new FileStream("c:/C sharp/aruna.txt", FileMode.Create, FileAccess.Read, FileShare.Read);
+            StreamWriter sw = new StreamWriter(obj);
+            StreamReader sr = new StreamReader(obj);
+            String s;
+            Console.WriteLine("Enter values:");
+            s = Console.ReadLine();
+            sw.Write(s);
+            String s1;
+            s1 = sr.ReadToEnd();
+            Console.WriteLine("values:" + s1);
+            sw.Close();
+            sr.Close();
+            Console.ReadLine();
+        }
+    }
+}
